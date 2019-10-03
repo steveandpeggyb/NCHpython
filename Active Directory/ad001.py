808ea6d6fe6b336c6c4e40eaf2015bac6267d0f2
Steve Blake <steveandpeggyb@hotmail.com>
2019-10-03 14:10:01 -0400

    .

--- Active Directory/ad001.py
+++ Active Directory/ad001.py
@@ -3,16 +3,16 @@ from ldap3 import *
 
 def main(snName):
     # Create the Server object with the given address.
-    server = Server('RPW-DC03.crii.org')    #, get_info=ALL)
+    server = Server('RPW-DC03.crii.org')
     
     #Create a connection object, and bind with the given DN and password.
     try: 
         conn = Connection(server, auto_bind=True)
-        print('LDAP Bind Successful.')
+        print('\n<<<<<<<<<<<<<<<<<<<<<<<<<< LDAP Bind Successful. >>>>>>>>>>>>>>>>>>>>>>>>>>')
         
         # Perform a search for a pre-defined criteria.
         # Mention the search filter / filter type and attributes.
-        conn.search('CN=Users', '(&(givenName=*Stev*)(sn=*Blak*))')
+        conn.search('CN=Users,DC=CRII,DC=ORG', '(sn=Blake)')
         
         # Print the resulting entries.
         print(conn.entries) # print(conn.entries[0])
@@ -20,7 +20,7 @@ def main(snName):
     except core.exceptions.LDAPBindError as e:
         
         # If the LDAP bind failed for reasons such as authentication failure.
-        print('LDAP Bind Failed: ', e) 
+        print('######################################################    LDAP Bind Failed: ', e) 
 
 
 print(main('blake'))

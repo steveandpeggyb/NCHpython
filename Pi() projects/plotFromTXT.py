bf2959a1647c61340fd7e879c5314f89af6c6467
Steve Blake <steveandpeggyb@hotmail.com>
2019-10-18 13:39:27 -0400

    .

--- Pi() projects/plotFromTXT.py
+++ Pi() projects/plotFromTXT.py
@@ -1,7 +1,8 @@
 from csb_navigate import calcVector
 import matplotlib.pyplot as plt
+import time
 
-filename = 'C:\\Users\\csb003\\Documents\\NCHpython\\Pi() projects\\100-PiDigits.txt'
+filename = 'C:\\Users\\csb003\\Documents\\NCHpython\\Pi() projects\\01m-PiDigits.txt'
 file = open(filename, mode='r')
 piTxt = file.read()
 file.close()
@@ -9,10 +10,25 @@ piTxt = piTxt.rstrip()
 
 xRaw, yRaw = calcVector(piTxt)
 
+
+
+
 # Plot the x and y values
 plt.xlabel('X')
 plt.ylabel('Y')
-plt.scatter(xRaw, yRaw, label='Legend Title', color='b', marker='.', s = 50)
+
+start = time.perf_counter()
+plt.scatter(xRaw, yRaw, label='Legend Title', color='b', marker='.', s = 1)
+# plt.plot(xRaw, yRaw, label='Legend Title', color='b', marker='.', markersize = 1, linestyle=':')
 plt.title('Interesting plot')
+
 plt.legend()
 plt.show()
+finish = time.perf_counter()
+
+if finish-start < 60:
+    t=str(round(finish-start, 3)) + ' seconds.'
+else:
+    t=str(round((finish-start)/60, 3)) + ' minutes.'
+
+print("Plotted {0:,} digits of Pi() in {1}\n".format(len(xRaw)-1, t))

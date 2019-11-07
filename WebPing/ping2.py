3be39f65a6cb538a471acac6a62b92345b84e382
Steve Blake <steveandpeggyb@hotmail.com>
2019-11-07 14:42:11 -0500

    .

--- WebPing/ping2.py
+++ WebPing/ping2.py
@@ -2,26 +2,28 @@ import subprocess
 
 def ping_ip(ip):
     (output, error) = subprocess.Popen((['ping', ip, '-w', '2']), stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE).communicate()
-
+    print(output)
     #for windows use -n instead of -c after ping, ip.
     if b'Reply from ' in output:
-        return "UP"
+        return ("UP", 1)
     elif b'Request timed out.' in output:
-        return "Request timed out."
+        return ("Request timed out.", 0)
     elif error:
-        return "DNS Error"
+        return ("DNS Error",0)
     else:
-        return "UNKNOWN"
+        return ("UNKNOWN",0)
 
 l = ''
 with open('./WebPing/devices.txt') as f:
     for ip in f:
         ip = ip.strip('\n')
-        response = ping_ip(ip)
-        result = ('%s\t%s \n' % (response, ip))
+        response, t = ping_ip(ip)
+        result = ('%s\t%i\t%s \n' % (ip, t, response))
         l = l+result
 
-print(l)
-file = open('output.txt', 'w')
+print('\r\n'+l)
+file = open('./WebPing/output.txt', 'w')
 file.write(l)
 file.close()
+
+print("Output.txt was created with these scanned results.")

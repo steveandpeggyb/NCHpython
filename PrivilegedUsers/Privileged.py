46885431aaadf4b1b8687ce155ea89eaaa538f97
Steve Blake <steveandpeggyb@hotmail.com>
2019-11-18 15:03:33 -0500

    .

--- PrivilegedUsers/Privileged.py
+++ PrivilegedUsers/Privileged.py
@@ -1,12 +1,16 @@
-import pyodbc 
-cnxn = pyodbc.connect("Driver={SQL Server Native Client 11.0};"
-                      "Server=\\RSW-BCRSQL01;"
-                      "Database=db_name;"
-                      "Trusted_Connection=yes;")
+import pyodbc
+PASSWORD = 'RQ*50XR!'
+
+
+cnxn = pyodbc.connect("Driver={ODBC Driver 17 for SQL Server};"
+                      "Server=RDW-BCRSQL01;"
+                      "Database=qpulse5;"
+                      "uid=BCRReporting;"
+                      "password=PASSWORD;")
 
 
 cursor = cnxn.cursor()
-cursor.execute('SELECT * FROM Table')
+cursor.execute('SELECT TOP 10 * from Person')
 
 for row in cursor:
     print('row = %r' % (row,))
--- PrivilegedUsers/SQL.py
+++ PrivilegedUsers/SQL.py
@@ -4,6 +4,7 @@ SERVERNAME = '\\RSW-BCRSQL01'
 DATA_BASE_INFO = 'Qpulse5'
 USERNAME = 'bcrReporting'
 PASSWORD = 'RQ*50XR!'
+print("setup the connection.")
 
 
 
@@ -15,8 +16,8 @@ PASSWORD = 'RQ*50XR!'
 
 
 
-
-con = sql.connect(driver="{SQL Server}",server=SERVERNAME,database=DATA_BASE_INFO,uid=USERNAME,password=PASSWORD, encoding='utf-8')
+con = sql.connect(driver="{ODBC Driver 17 for SQL Server}",server=SERVERNAME,database=DATA_BASE_INFO,uid=USERNAME,password=PASSWORD, encoding='utf-8')
+print("Line 20")
 cur = con.cursor()
 db_cmd = "SELECT TOP 10 FullName, JobTitle, ActiveDirectoryAlias FROM [QPulse5].[dbo].[Person] where IsDeleted = 0 AND IsUser = 1"
 res = cur.execute(db_cmd)

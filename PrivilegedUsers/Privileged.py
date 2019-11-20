1d3bc0b5af8314b54f41aeaf5f985c32e5f6c75a
Steve Blake <steveandpeggyb@hotmail.com>
2019-11-20 14:53:03 -0500

    Added Search and email to main module

--- PrivilegedUsers/Privileged.py
+++ PrivilegedUsers/Privileged.py
@@ -1,7 +1,18 @@
-from QueryADlibrary import SearchAD
 from datetime import datetime
 from email.mime.text import MIMEText
 import smtplib
+import pyad.adquery as adquery
+import pprint
+
+def SearchAD(username='csb003'):
+
+    q = adquery.ADQuery()
+    q.execute_query(
+        attributes = ["cn", "member", "sAMAccountType", "sAMAccountName"],
+        where_clause=("sAMAccountName = '" + username + "'"),
+    )
+
+    return q.get_results()
 
 def send_email(subject, body, to):
     s = smtplib.SMTP('xmail.nationwidechildrens.org')

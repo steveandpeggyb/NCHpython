1c8108a7a1191b8edc8fa30202369c876544cb9d
Steve Blake <steveandpeggyb@hotmail.com>
2019-11-20 14:59:50 -0500

    Ran through a series

--- PrivilegedUsers/Privileged.py
+++ PrivilegedUsers/Privileged.py
@@ -25,35 +25,39 @@ subj = 'Privileged User Access - ' + str(datetime.today())
 send_to = 'steve.blake@nationwidechildrens.org'
 
 username = "bpc"
+usernames = ('bcr', 'csb003', 'bpc', 'txb053', 'cra030')
 
-results = SearchAD(username)
-i=1
-body_text = ''
-for r in results:
-    i += 1
-    CN = r['cn']
-    MEMBER = r['member']
-    sAMAccountType = r['sAMAccountType']
-    sAMAccountName = r['sAMAccountName']
+for index in range(len(usernames)):
+    username = usernames[index]
 
-body_text = body_text + '\r\nCN:\t\t\t'+ CN + '\r\n'
-body_text = body_text + 'sAMAccountType:\t'+str(sAMAccountType) + '\r\n'
-body_text = body_text + 'sAMAccountName:\t'+sAMAccountName + '\r\n'
-body_text = body_text + 'Member of Group:\r\n'
-if MEMBER == None:
-    body_text = body_text + '\tGroup has no members.'
-else:
-    for index in range(len(MEMBER)):
-        body_text = body_text + '\t' + str(index+1) + '\t' + MEMBER[index] + '\r\n'
-print()
+    results = SearchAD(username)
+    i=1
+    body_text = ''
+    for r in results:
+        i += 1
+        CN = r['cn']
+        MEMBER = r['member']
+        sAMAccountType = r['sAMAccountType']
+        sAMAccountName = r['sAMAccountName']
 
-body_text = body_text.replace('CN=','')
-body_text = body_text.replace(',DC=CRII,DC=ORG','')
-body_text = body_text.replace(',OU=CRI DLs,OU=ExchangeMigration','')
-body_text = body_text.replace(',OU=BCR','')
-body_text = body_text.replace(',OU=BPC','')
-body_text = body_text.replace(',Users','')
-body_text = body_text.replace('\,',',')
+    body_text = body_text + '\r\nCN:\t\t\t'+ CN + '\r\n'
+    body_text = body_text + 'sAMAccountType:\t'+str(sAMAccountType) + '\r\n'
+    body_text = body_text + 'sAMAccountName:\t'+sAMAccountName + '\r\n'
+    body_text = body_text + 'Member of Group:\r\n'
+    if MEMBER == None:
+        body_text = body_text + '\tGroup has no members.'
+    else:
+        for index in range(len(MEMBER)):
+            body_text = body_text + '\t' + str(index+1) + '\t' + MEMBER[index] + '\r\n'
+    print()
 
-# send_email(subj, body_text, send_to)
-print("\r\n" + body_text.replace('\t\t\t','\t\t') + "\r\n")
+    body_text = body_text.replace('CN=','')
+    body_text = body_text.replace(',DC=CRII,DC=ORG','')
+    body_text = body_text.replace(',OU=CRI DLs,OU=ExchangeMigration','')
+    body_text = body_text.replace(',OU=BCR','')
+    body_text = body_text.replace(',OU=BPC','')
+    body_text = body_text.replace(',Users','')
+    body_text = body_text.replace('\,',',')
+
+    # send_email(subj, body_text, send_to)
+    print("\r\n" + body_text.replace('\t\t\t','\t\t') + "\r\n")

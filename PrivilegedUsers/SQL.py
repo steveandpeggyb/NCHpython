9e9eb108484497d56fcaf4ed40bdb8f6dda535fa
Steve Blake <steveandpeggyb@hotmail.com>
2019-11-21 15:09:26 -0500

    .

--- PrivilegedUsers/SQL.py
+++ PrivilegedUsers/SQL.py
@@ -5,6 +5,17 @@ DATA_BASE_INFO = 'Qpulse5'
 USERNAME = 'QPulseAppUser'
 PASSWORD = 'Gr1mjd#12'
 
+
+
+
+
+
+
+
+
+
+
+
 print("\r\n")
 
 con = sql.connect(driver="{ODBC Driver 17 for SQL Server}",server=SERVERNAME,database=DATA_BASE_INFO,uid=USERNAME,password=PASSWORD, encoding='utf-8')
@@ -12,13 +23,14 @@ cur = con.cursor()
 db_cmd = "SELECT TOP 10 FullName, JobTitle, ActiveDirectoryAlias FROM [QPulse5].[dbo].[Person] where IsDeleted = 0 AND IsUser = 1"
 res = cur.execute(db_cmd)
 
-
-
 # Do something with your result set, for example print out all the results:
-for r in res:
-    print(r)
+if res == None:
+    print("No data returned!")
+else:
+    for r in res:
+        print(r)
 
 print("\r\n")
 
-sql.close()
+# sql.close()
 

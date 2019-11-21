9706482b1ebe072c12c7d362bdc9eeb332f27326
Steve Blake <steveandpeggyb@hotmail.com>
2019-11-21 08:10:33 -0500

    SQL was return data

--- PrivilegedUsers/SQL.py
+++ PrivilegedUsers/SQL.py
@@ -1,27 +1,22 @@
 import pyodbc as sql
 # Create connection
-SERVERNAME = '\\RSW-BCRSQL01'
+SERVERNAME = 'RDW-BCRSQL01'
 DATA_BASE_INFO = 'Qpulse5'
 USERNAME = 'QPulseAppUser'
 PASSWORD = 'Gr1mjd#12'
-print("setup the connection.")
 
 
-
-
-
-
-
-
-
-
+print("\r\n")
 
 con = sql.connect(driver="{ODBC Driver 17 for SQL Server}",server=SERVERNAME,database=DATA_BASE_INFO,uid=USERNAME,password=PASSWORD, encoding='utf-8')
-print("Line 20")
 cur = con.cursor()
 db_cmd = "SELECT TOP 10 FullName, JobTitle, ActiveDirectoryAlias FROM [QPulse5].[dbo].[Person] where IsDeleted = 0 AND IsUser = 1"
 res = cur.execute(db_cmd)
 # Do something with your result set, for example print out all the results:
 for r in res:
     print(r)
+
+print("\r\n")
+
 sql.close()
+

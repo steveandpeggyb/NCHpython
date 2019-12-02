2b4ea54d9e5aeb33b4301ec72e2edb51574b08f8
Steve Blake <steveandpeggyb@hotmail.com>
2019-12-02 08:07:39 -0500

    update title with comma digits

--- Pi() projects/present.py
+++ Pi() projects/present.py
@@ -1,4 +1,6 @@
 # from numpy import zeros
+import matplotlib.pyplot as plt
+import pandas
 
 f = open("C:\\Users\\csb003\\Documents\\NCHpython\\Pi() projects\\10k-DigitsPi.txt", "r")
 piData = f.read()
@@ -8,7 +10,7 @@ f.close()
 xDict = {'0': 0, '1': 1.7633558, '2': 2.8531695, '3': 2.8531695, '4': 1.7633558, '5': 0, '6': -1.7633558, '7': -2.8531695, '8': -2.8531695, '9': -1.7633558}
 yDict = {'0': 3, '1': 2.427051, '2': 0.927051, '3': -0.927051, '4': -2.427051, '5': -3, '6': -2.427051, '7': -0.927051, '8': 0.927051, '9': 2.427051}
 
-Output = []
+x, y = [],[]
 NewX = 0
 NewY = 0
 
@@ -16,9 +18,21 @@ for digit in piData:
     if digit not in ('.', '\n'):
         NewX = xDict[digit]+NewX
         NewY = yDict[digit]+NewY
-        Output.append([NewX, NewY])
+        x.append(NewX)
+        y.append(NewY)
 
-# for e in a:
-#     print(e)
+#   Plot the data
+
+fig, ax = plt.subplots()
+ax.plot(x, y)
+
+Title = "{:,}".format(len(x)-1) + " Digits of PI()"
+
+ax.set(xlabel='X - Axis', ylabel='Y - Axis', title= Title)
+ax.grid()
+
+fig.savefig("C:\\Users\\csb003\\Documents\\NCHpython\\Pi() projects\\test.png")
+
+plt.show() 
 
 print("All Done!")

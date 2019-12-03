5438b4d171bcf9d92beed8b9001c2bf74f84acc6
Steve Blake <steveandpeggyb@hotmail.com>
2019-12-03 11:45:14 -0500

    .

--- Pi() projects/present.py
+++ Pi() projects/present.py
@@ -2,7 +2,7 @@
 import matplotlib.pyplot as plt
 import pandas
 
-f = open("C:\\Users\\csb003\\Documents\\NCHpython\\Pi() projects\\10k-DigitsPi.txt", "r")
+f = open("C:\\Users\\csb003\\Documents\\NCHpython\\Pi() projects\\10m-PiDigits.txt", "r")
 piData = f.read()
 f.close()
 
@@ -10,7 +10,7 @@ f.close()
 xDict = {'0': 0, '1': 1.7633558, '2': 2.8531695, '3': 2.8531695, '4': 1.7633558, '5': 0, '6': -1.7633558, '7': -2.8531695, '8': -2.8531695, '9': -1.7633558}
 yDict = {'0': 3, '1': 2.427051, '2': 0.927051, '3': -0.927051, '4': -2.427051, '5': -3, '6': -2.427051, '7': -0.927051, '8': 0.927051, '9': 2.427051}
 
-x, y = [],[]
+x, y = [0],[0]
 NewX = 0
 NewY = 0
 
@@ -24,12 +24,15 @@ for digit in piData:
 #   Plot the data
 
 fig, ax = plt.subplots()
-ax.plot(x, y)
+
+# https://www.w3schools.com/colors/colors_xkcd.asp
+ax.plot(x, y, 'xkcd:indigo')
 
 Title = "{:,}".format(len(x)-1) + " Digits of PI()"
 
-ax.set(xlabel='X - Axis', ylabel='Y - Axis', title= Title)
-ax.grid()
+ax.set_axis_off()
+ax.set(title= Title)
+ax.grid(False)
 
 fig.savefig("C:\\Users\\csb003\\Documents\\NCHpython\\Pi() projects\\test.png")
 

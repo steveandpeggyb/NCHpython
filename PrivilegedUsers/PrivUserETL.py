6211b57a94cb3ccab20abff09de7b2b2ba34df8e
Steve Blake <steveandpeggyb@hotmail.com>
2019-12-03 13:27:51 -0500

    Extracting data completed Note: need to address "Hospital" and "nt service"

---
+++ PrivilegedUsers/PrivUserETL.py
@@ -0,0 +1,21 @@
+def UserData():
+    import csv
+
+    with open ("R:/RESBCR/FISMA/PrivilegedUserAudits/PrivilegedUserAudit.csv") as f:
+        reader = csv.reader(f)
+        next(reader) # skip header
+        data = []
+        for row in reader:
+            if len(row)>0:
+                # data.append(row)
+                sandbox = row[0]
+                sandbox = sandbox.lower()
+                sandbox = sandbox.replace("research\\","")
+                sandbox = sandbox.strip()
+                data.append(sandbox)
+        return data
+
+for row in UserData():
+    print(row)
+
+print()

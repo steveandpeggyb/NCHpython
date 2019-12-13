117955bff5c1e70da053fefe40daf518adb82d8f
Steve Blake <steveandpeggyb@hotmail.com>
2019-12-13 11:54:15 -0500

    loop through all plot styles

--- matplotlibexamples/mpl001.py
+++ matplotlibexamples/mpl001.py
@@ -5,33 +5,36 @@ from itertools import count
 import pandas as pd
 import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
+plt.style.available[:26]
 
-plt.style.use('fivethirtyeight')
+loop = len(plt.style.available[:])
+for x in range(loop):
+    plt.style.use(plt.style.available[x:x+1])
 
-x_vals = []
-y_vals = []
+    x_vals = []
+    y_vals = []
 
-index = count()
+    index = count()
 
 
-def animate(i):
-    data = pd.read_csv('c:\\TEMP\\data.csv')
-    x = data['x_value']
-    y1 = data['total_1']
-    y2 = data['total_2']
+    def animate(i):
+        data = pd.read_csv('c:\\TEMP\\data.csv')
+        x = data['x_value']
+        y1 = data['total_1']
+        y2 = data['total_2']
 
-    plt.cla()
+        plt.cla()
 
-    plt.plot(x, y1, label='Channel 1')
-    plt.plot(x, y2, label='Channel 2')
+        plt.plot(x, y1, label='Channel 1')
+        plt.plot(x, y2, label='Channel 2')
 
-    plt.legend(loc='upper left')
+        plt.legend(loc='upper left')
+        plt.tight_layout()
+
+
+    ani = FuncAnimation(plt.gcf(), animate, interval=500)
+
     plt.tight_layout()
-
-
-ani = FuncAnimation(plt.gcf(), animate, interval=500)
-
-plt.tight_layout()
-plt.show()
+    plt.show()
 
 

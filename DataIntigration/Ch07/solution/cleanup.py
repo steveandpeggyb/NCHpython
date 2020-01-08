f34c4f67efda436eaeb7d9caeaec4768fb5591f8
Steve Blake <steveandpeggyb@hotmail.com>
2020-01-08 12:22:49 -0500

    .

--- DataIntigration/Ch07/solution/cleanup.py
+++ DataIntigration/Ch07/solution/cleanup.py
@@ -5,7 +5,7 @@ import sqlite3
 import pandas as pd
 
 
-conn = sqlite3.connect('rides.db', detect_types=sqlite3.PARSE_DECLTYPES)
+conn = sqlite3.connect('C:/Users/csb003/Documents/NCHpython/DataIntigration/Ch07/solution/rides.db', detect_types=sqlite3.PARSE_DECLTYPES)
 df = pd.read_sql('SELECT * FROM rides', conn)
 df['duration'] = df['tpep_dropoff_datetime'] - df['tpep_pickup_datetime']
 print('min:', df['duration'].min(), 'max:', df['duration'].max())

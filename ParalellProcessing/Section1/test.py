a00d40bba36804de8eaab53beb9fc3c6906a16e9
Steve Blake <steveandpeggyb@hotmail.com>
2020-01-23 12:54:15 -0500

    .

--- ParalellProcessing/Section1/test.py
+++ ParalellProcessing/Section1/test.py
@@ -1,3 +1,4 @@
+#   https://www.lynda.com/Python-tutorials/Introducing-Python/604237/622567-4.html
 x = int(input("Please enter an integer here."))
 if x<0:
     print('the number is negative')
--- ParalellProcessing/Section1/test2.py
+++ ParalellProcessing/Section1/test2.py
@@ -1,3 +1,4 @@
+#   https://www.lynda.com/Python-tutorials/Introducing-Python/604237/622567-4.html
 def fib(n):
     """ Print a Fibonacci series upto n"""
     a, b = 0,1
